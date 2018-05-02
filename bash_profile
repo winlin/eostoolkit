9454c5a465a01aa36b772cd1341d6fa315e326ef
winlin <pcliuguangtao@163.com>
2018-05-02 17:25:55 +0800

    init the repository

---
+++ bash_profile
@@ -0,0 +1,2 @@
+alias cleos="sudo docker exec YOUR_NODEOSD_DOCKERNAME /usr/local/bin/cleos"
+alias eosiocpp="sudo docker exec --workdir /opt/eosio/bin/data-dir/contracts/ YOUR_NODEOSD_DOCKERNAME /usr/local/bin/eosiocpp"
---
+++ eosbpinit.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+EOS_TAG="DAWN-2018-04-23-ALPHA"
+
+INSTALL_APPS=(
+    "tmux"
+    "axel"
+    "git"
+    "apt-transport-https"
+    "ca-certificates"
+    "curl"
+    "software-properties-common"
+    "python-pip"
+    "tree"
+    )
+
+PIP_APPS=(
+    "requests"
+    "docker-compose"
+    )
+
+APP_REPO=(
+    "ppa:git-core/ppa"
+    #"deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+    )
+
+GPG_KEYS=(
+    #"https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg"
+    )
+
+INIT_PATH="${HOME}/inithost"
+mkdir -p $INIT_PATH
+cd $INIT_PATH
+if [[ $? -ne 0 ]]; then
+    echo "FAILED to cd $INIT_PATH"
+    exit 1
+fi
+
+
+if [[ $1 != "init" && $1 != "source" && $1 != "docker" && $1 != "pull" ]]; then
+    echo "Usage: $0 init|pull|source|docker"
+    echo "      init: just initialize the machine"
+    echo "      pull: just pull the latest code for EOSIO"
+    echo "      source: just pull the latest code for EOSIO and call eosio_build.sh"
+    echo "      docker: just pull the latest code for EOSIO and wait for docker build"
+    exit 1
+fi
+
+
+function install_docker() {
+    curl -fsSL get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh --mirror Aliyun
+
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    sudo usermod -aG docker $USER
+}
+
+function init_pip() {
+    echo ">>>>>>>>>>> install PIP apps"
+    sudo pip install --upgrade pip
+    for item in "${PIP_APPS[@]}"; do
+        sudo pip install "$item" --upgrade
+    done
+}
+
+function init_host() {
+    echo ">>>>>>>>>>> install GPG Keys"
+    for item in "${GPG_KEYS[@]}"; do
+        curl -fsSL "$item" | sudo apt-key add -
+    done
+
+    echo ">>>>>>>>>>> install repositories"
+    for item in "${APP_REPO[@]}"; do
+        sudo apt-add-repository "$item"
+    done
+    sudo apt-get update
+
+    echo ">>>>>>>>>>> install apps"
+    for item in "${INSTALL_APPS[@]}"; do
+        sudo apt-get install -y "$item"
+        if [[ $? -ne 0 ]]; then
+            echo "FAILED to install $item"
+            exit 1
+        fi
+    done
+
+    install_docker
+    init_pip
+}
+
+function pull_eossrc() {
+    mkdir -p ~/opt/
+    cd ~/opt/
+    if [[ ! -e eos ]]; then
+        git clone https://github.com/EOSIO/eos --recursive
+        if [[ $? -ne 0 ]]; then
+            echo "FAILED to get eos code"
+            exit 1
+        fi
+    fi
+    cd eos
+    git checkout scripts/eosio_build_ubuntu.sh
+    git checkout master --recurse-submodules
+    git pull origin --recurse-submodules
+    git submodule update --init --recursive
+}
+
+function build_eossrc() {
+    pull_eossrc
+    cd ~/opt/eos/
+    git checkout tags/$EOS_TAG -b $EOS_TAG --recurse-submodules
+    if [[ $? -ne 0 ]]; then
+        echo "FAILED to get checkout tag $EOS_TAG"
+        exit 1
+    fi
+    git submodule update --init --recursive
+    ./eosio_build.sh
+    if [[ $? -ne 0 ]]; then
+        echo "FAILED to build EOS with tag $EOS_TAG"
+        exit 1
+    fi
+    cd build
+    make test
+    if [[ $? -ne 0 ]]; then
+        echo "FAILED to make test EOS with tag $EOS_TAG"
+        exit 1
+    fi
+    sudo make install
+}
+
+function build_eosdocker() {
+    pull_eossrc
+    cd ~/opt/eos/
+    git branch -d $EOS_TAG
+    git checkout tags/$EOS_TAG -b $EOS_TAG --recurse-submodules
+    if [[ $? -ne 0 ]]; then
+        echo "FAILED to get checkout tag $EOS_TAG"
+        exit 1
+    fi
+    git submodule update --init --recursive
+    if [[ $? -ne 0 ]]; then
+        echo "FAILED to fetch latest code with tag $EOS_TAG"
+        exit 1
+    fi
+}
+
+if [[ $1 == "init" ]]; then
+    init_host
+    exit 0
+fi
+
+if [[ $1 == "pull" ]]; then
+    pull_eossrc
+    exit 0
+fi
+
+EOS_TAG=$2
+if [[ $EOS_TAG == "" ]]; then
+    echo "Please supply the target tag name"
+    exit 1
+fi
+echo "Going to checkout tag: ${EOS_TAG}"
+if [[ $1 == "source" ]]; then
+    build_eossrc
+    exit 0
+fi
+if [[ $1 == "docker" ]]; then
+    build_eosdocker
+    exit 0
+fi
