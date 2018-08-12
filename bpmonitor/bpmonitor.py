4e5aa7f45d6c92832ae024687f879c0638863faf
winlin <pcliuguangtao@163.com>
2018-08-12 23:09:03 +0800

    bpmonitor.py upgrade

--- bpmonitor/bpmonitor.py
+++ bpmonitor/bpmonitor.py
@@ -19,23 +19,16 @@ from threading import Thread
 import simplejson as json
 from decimal import Decimal
 
-try:
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-except NameError:
-    pass
-except Exception as err:
-    raise err
-
 # Just re-write yourself warning function body, nothing else need to be change.
 def send_warning(msg, config_dict, sms_flag):
     ''' msg: the message need to send 
         config_dict: the configures read from the bpmonitor.json
     '''
     # replace with yourself warning function 
-    send_dingding_msg(msg, config_dict)
     if sms_flag:
-        send_mobile_msg(msg, config_dict)
+       send_mobile_msg(msg, config_dict)
+    send_dingding_msg(msg, config_dict)
+    send_telegram_msg(msg, config_dict)
     
 # Send Dingtalk
 def send_dingding_msg(msg, config_dict):
@@ -57,21 +50,40 @@ def send_dingding_msg(msg, config_dict):
         print 'get_current_bp get exception:', e
         print traceback.print_exc()
 
+# Send Telegram Bot
+def send_telegram_msg(msg, config_dict):
+    try:
+        url = "https://api.telegram.org/bot%s/sendMessage" % (config_dict["telegram"]["token"])
+        param = {"chat_id":config_dict["telegram"]["chat_id"], "text":msg, }
+        result = requests.post(url, param, timeout=5.0)
+        print "telegram_alarm send message:", msg, result.text
+    except Exception as e:
+        print 'get_current_bp get exception:', e
+        print traceback.print_exc()
+
+# Send SMS
 def send_mobile_msg(msg, config_dict):
-    sign = '请及时处理。【EOS主网运维】'
+    send_phones = []
+    for key in config_dict['notify_phones']:
+        if key == '*':
+            send_phones.extend(config_dict['notify_phones'][key])
+            continue
+        if key.lower() in msg:
+            send_phones.extend(config_dict['notify_phones'][key])
+            continue
+    send_phones = set(send_phones)
+    print 'Send to :', send_phones
+    sign = ' [EOSBIXIN]'
     querystring = {"action": "send",
                    "userid": config_dict['yqq_userid'],
                    "account": config_dict['yqq_account'],
                    "password": config_dict['yqq_password'],
-                   "mobile": ",".join(config_dict['phones']),
+                   "mobile": ",".join(send_phones),
                    "content": msg + sign,
                    "sendTime": "", "extno": ""}
-
     headers = {
         'Cache-Control': "no-cache",
-        'Postman-Token': "0bca5e96-cd0e-4768-8b44-112ec66336ea"
     }
-
     try:
         res = requests.request("GET", config_dict['yqq_smsurl'], headers=headers, params=querystring)
         rj = json.loads(res.content)
@@ -80,36 +92,13 @@ def send_mobile_msg(msg, config_dict):
         print 'get_current_bp get exception:', e
         print traceback.print_exc()
 
-# Send SMS
-def send_alisms(msg, config_dict):
-    try:
-        from aliyunsdkdysmsapi.request.v20170525 import SendSmsRequest
-        from aliyunsdkcore.client import AcsClient
-        from aliyunsdkcore.profile import region_provider
-
-        REGION = "cn-hangzhou"
-        PRODUCT_NAME = "Dysmsapi"
-        DOMAIN = "dysmsapi.aliyuncs.com"
-        acs_client = AcsClient(config_dict['accessid'], config_dict['accessidkey'], REGION)
-        region_provider.add_endpoint(PRODUCT_NAME, REGION, DOMAIN)
-        smsRequest = SendSmsRequest.SendSmsRequest()
-        smsRequest.set_TemplateCode(config_dict['template_code'])
-        template_param = '{"timestamp":"%s","account":"%s","offlinetime":"","message":"%s"}' % (
-            second2_str24h(time.time(), fmt="%H:%M:%S"), config_dict['bpaccount'], msg[:20]
-        )
-        smsRequest.set_TemplateParam(template_param)
-        smsRequest.set_OutId(uuid.uuid1())
-        smsRequest.set_SignName(config_dict['signname'])
-        smsRequest.set_PhoneNumbers(",".join(config_dict['phones']))
-        print acs_client.do_action_with_exception(smsRequest)
-    except Exception as e:
-        print 'get_current_bp get exception:', e
-        print traceback.print_exc()
-
 ############################################
 g_stop_thread = False
 g_notify_cache = {}
-g_http_timeout = 1.5
+
+HTTP_TIMEOUT = 6
+HTTP_AGENT = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}
+
 def notify_users(msg, config_dict, sms_flag=False):
     global g_notify_cache
     try:
@@ -117,6 +106,7 @@ def notify_users(msg, config_dict, sms_flag=False):
         if msg_hash in g_notify_cache and (time.time() - g_notify_cache[msg_hash] < config_dict['notify_interval']):
             return
         g_notify_cache[msg_hash] = time.time()
+        print msg
         send_warning(msg, config_dict, sms_flag)
     except Exception as e:
         print 'get_bpinfo get exception:', e
@@ -132,83 +122,150 @@ def second2_str24h(cur_second, fmt="%Y-%m-%d %H:%M:%S", utc=False):
         return time.strftime(fmt, time.localtime(cur_second))
     return time.strftime(fmt, time.gmtime(cur_second))
 
+def datestr24h_2second(date_str):
+    return int(time.mktime(time.strptime(date_str, "%Y-%m-%dT%H:%M:%S")))
+
 def get_current_bp(host):
     try:
         get_info = "%s/v1/chain/get_info" % (host)
-        ret = requests.get(get_info, timeout=g_http_timeout, 
+        ret = requests.get(get_info, timeout=HTTP_TIMEOUT, 
                     headers={'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"})
         if ret.status_code/100 != 2:
             print 'ERROR: failed to call:', get_info, ret.text
             return None, 'get_current_bp failed:' + host
         result = json.loads(ret.text)
-        return result['head_block_producer'], None
+        return result, None
     except Exception as e:
         print 'get_current_bp get exception:', e
         print traceback.print_exc()
     return None, 'get_current_bp get exception:' + host
 
-def get_bpinfo(host, account):
+def get_bp_order(host):
     try:
-        top_limit = 50
+        top_limit = 30
         data = '{ "json": true, "lower_bound": "", "limit": %d}' % top_limit
         list_prods = "%s/v1/chain/get_producers" % (host)
-        ret = requests.post(list_prods, data=data, timeout=g_http_timeout, 
+        ret = requests.post(list_prods, data=data, timeout=HTTP_TIMEOUT, 
                     headers={'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"})
         if ret.status_code/100 != 2:
             print 'ERROR: failed to call:', list_prods, ret.text
             return None, 'get_producers failed:' + host
         result = json.loads(ret.text)
-        account_info = {'vote_rate':0.0, 'account':account,}
-        for index,item in enumerate(result['rows']):
-            if item['owner'] != account:
-                continue
-            account_info['vote_rate'] = float(Decimal(item['total_votes']) / Decimal(result['total_producer_vote_weight']))
-            account_info['rank'] = index + 1
-            return account_info, None
-        return None, 'You have out of top %d' % (top_limit)
+        bps_rank = []
+        for index, item in enumerate(result['rows']):
+            if index>20:
+                break
+            bps_rank.append(item["owner"])
+            if time.time() - int(item['last_claim_time'])/1000000 > 3600*24.1:
+                #daily claim delay
+                msg = "%s claimreward delay for more than 6min" % (item["owner"])
+                notify_users(msg, config_dict, sms_flag=True)
+        return sorted(bps_rank), None
     except Exception as e:
-        print 'get_bpinfo get exception:', e
+        #pass
+        print 'get_bp_order get exception:', e
         print traceback.print_exc()
-    return None, 'get_bpinfo get exception:' + host
+    return None, 'get_bp_order get exception:' + host 
+
+def get_block_producer(host, num):
+    try:
+        url = host + "/v1/chain/get_block"
+        payload = "{\"block_num_or_id\":%d}" % num
+        response = requests.post(url, data=payload, timeout=HTTP_TIMEOUT,
+                        headers={'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"})
+        bpline = response.text[:64] + '}'
+        print 'Get block %d :%s' % (num, bpline)
+        return json.loads(bpline), None
+    except Exception as e:
+        pass
+        #print 'get_block_producer get exception:', e
+        #print traceback.print_exc()
+    return None, 'get_block_producer get exception:' + host 
+
+def check_nextbp_legal(bp_order, pre_bp, cur_bp):
+    try:
+        if bp_order[bp_order.index(pre_bp)+1] != cur_bp:
+            return False, bp_order[bp_order.index(pre_bp)+1]
+    except Exception as e:
+        pass
+    # The first 21 bps order may change, so here just assume legal
+    return True, None
+
+def check_bprank_change(pre_rank, cur_rank, config_dict):
+    outrank_bps = set(pre_rank) - set(cur_rank)
+    for bp in outrank_bps:
+        msg = "%s out rank of %d" % (bp, len(cur_rank))
+        notify_users(msg, config_dict, sms_flag=True)
+
+    for bp in cur_rank:
+        if cur_rank.index(bp) != pre_rank.index(bp):
+            msg = "%s rank changed from %d to %d" % (bp, pre_rank.index(bp), cur_rank.index(bp))
+            notify_users(msg, config_dict, sms_flag=True)
 
 def check_rotating(host, status_dict, config_dict):
     global g_stop_thread
     if host not in status_dict:
         status_dict[host] = {'vote_rates_24h': collections.deque(maxlen=24), 'rank_last_2':collections.deque(maxlen=2), 'last_cblock_time':time.time()}
+    
+    pre_bp, cur_bp, bp_order = None, None, None
+    curbp_bcount, lib_num, cur_lib_num, start_lib_num = 0, 0, 0, 0
+    rotate_time, pre_bprank = time.time(), None
     while not g_stop_thread:
         try:
-            # check the bp rank
-            bpinfo, err = get_bpinfo(host, config_dict['bpaccount'])
+            curbp_info, err = get_current_bp(host)
             if err:
                 notify_users(err, config_dict, sms_flag=False)
+                rotate_time = time.time()
                 continue
-            if int(time.time()) % 3600 == 0:  # store the vote rate per hour
-                status_dict[host]['vote_rates_24h'].append(bpinfo['vote_rate'])
-            status_dict[host]['rank_last_2'].append(bpinfo['rank'])
-            if len(status_dict[host]['rank_last_2'])>1 and status_dict[host]['rank_last_2'][0] != status_dict[host]['rank_last_2'][1]:
-                msg = "Rank changd: from %d to %d" % (status_dict[host]['rank_last_2'][1], status_dict[host]['rank_last_2'][0])
-                notify_users(msg, config_dict, sms_flag=True)
+            lib_num = curbp_info['last_irreversible_block_num']
+            if cur_lib_num < 1:
+                cur_lib_num, start_lib_num = lib_num, lib_num
+            if cur_lib_num > lib_num:
+                # wait for LIB increase
+                rotate_time = time.time()
+                continue
 
-            # update monitor account last create block timestamp
-            if bpinfo['rank'] < 22 and int(time.time()) % 3 == 0:   # get create block account per 3 secs
-                curbp, err = get_current_bp(host)
-                print second2_str24h(time.time()), curbp, host
+            block_bpinfo, err = get_block_producer(host, cur_lib_num)
+            if err:
+                notify_users(err, config_dict, sms_flag=False)
+                rotate_time = time.time()
+                continue
+            cur_bp = block_bpinfo['producer']
+            if not pre_bp:
+                pre_bp = cur_bp
+            if pre_bp != cur_bp:
+                bp_order, err = get_bp_order(host)
                 if err:
                     notify_users(err, config_dict, sms_flag=False)
-                if curbp == config_dict['bpaccount']:
-                    status_dict[host]['last_cblock_time'] = time.time()
+                    rotate_time = time.time() + 1.0
+                    continue
+                if not pre_bprank:
+                    pre_bprank = bp_order
+                check_bprank_change(pre_bprank, bp_order, config_dict)
+                pre_bprank = bp_order
+
+            cur_lib_num += 1
+            if pre_bp == cur_bp:
+                curbp_bcount += 1
+                continue
+        
+            legal, legal_bp = check_nextbp_legal(bp_order, pre_bp, cur_bp) 
+            if not legal:
+                msg = "%s MIGHT miss 12 blocks after %d" % (legal_bp, cur_lib_num-1)
+                notify_users(msg, config_dict, sms_flag=True)
+
+            if curbp_bcount<11 and cur_lib_num-start_lib_num>11:
+                msg = "%s [%d - %d] miss %d blocks " % (pre_bp, cur_lib_num-2-curbp_bcount, cur_lib_num-1, 12-curbp_bcount)
+                notify_users(msg, config_dict, sms_flag=True)
+            curbp_bcount = 1
+            pre_bp = cur_bp
         except Exception as e:
             print 'check_rotating get exception:', e
             print traceback.print_exc()
         finally:
-            time.sleep(0.5)
-
-def calc_day_reward(vote_rate, rank):
-    reward_eos = 0
-    if rank < 22:
-        reward_eos += 1000000000*0.01*0.25/365/21
-    reward_eos += 1000000000*0.01*0.75/365 * vote_rate
-    return reward_eos
+            sleep_time = 2.0 + rotate_time - time.time()
+            if sleep_time > 0:
+                time.sleep(sleep_time)
 
 def signal_default_handler(sig, frame):
     global g_stop_thread
@@ -218,41 +275,7 @@ def signal_default_handler(sig, frame):
 def main(config_dict):
     global g_stop_thread
     status_dict = {}
-    threadlist = []
-
-    for host in config_dict['http_urls']:
-        th = Thread(target=check_rotating, args=(host, status_dict, config_dict))
-        threadlist.append(th)
-        th.start()
-
-    reward_flags, latest_cblock_time = collections.deque(maxlen=5), time.time()
-    while not g_stop_thread:
-        time.sleep(1)
-        need_check_cblock = False
-        for host in status_dict:
-            rate_len = len(status_dict[host]['vote_rates_24h'])
-            if rate_len == 0:
-                continue
-            latest_cblock_time = max(latest_cblock_time, status_dict[host]['last_cblock_time'])
-            need_check_cblock = status_dict[host]['rank_last_2'][0] < 22 
-            reward_flag = int(time.time()) / (3600*24)     # store one day's average reward
-            if int(time.time()) % (3600*24) == 0:
-                if reward_flag in reward_flags:
-                    continue
-                reward_flags.append(reward_flag)
-                with open(config_dict['reward_output'], "a") as fp:
-                    day_str = second2_str24h(int(time.time()))
-                    day_avg_rate = sum(status_dict[host]['vote_rates_24h']) * 1.0 / len(status_dict[host]['vote_rates_24h'])
-                    day_reward = calc_day_reward(day_avg_rate, status_dict[host]['rank_last_2'][0])
-                    fp.write("%s,%f,%f\n" % (day_str, day_avg_rate, day_reward))
-
-        if need_check_cblock and time.time() - latest_cblock_time > 130:  # rotate period is 120 secs
-            msg = "Long time NO create blocks, lasttime: %s " % second2_str24h(latest_cblock_time)
-            print second2_str24h(time.time()), msg
-            notify_users(msg, config_dict, sms_flag=True)
-    
-    for th in threadlist:
-        th.join()
+    check_rotating(config_dict['http_urls'][0], status_dict, config_dict)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
